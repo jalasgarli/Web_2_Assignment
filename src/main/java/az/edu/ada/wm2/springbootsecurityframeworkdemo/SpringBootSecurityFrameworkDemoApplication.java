c880ec39c691580fbb1984a032c33d7e87a896a3
javidalasgarli <alasgarlijavid@gmail.com>
2024-05-08 17:35:55 +0400

    Assignment 2'

---
+++ src/main/java/az/edu/ada/wm2/springbootsecurityframeworkdemo/SpringBootSecurityFrameworkDemoApplication.java
@@ -0,0 +1,14 @@
+package az.edu.ada.wm2.springbootsecurityframeworkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringBootSecurityFrameworkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityFrameworkDemoApplication.class, args);
+	}
+
+}
